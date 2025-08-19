fc9f538939c0beea0a4906cf4a115789e24bb3f3
EZRA OLUYOYIN O. <141941097+tamval15@users.noreply.github.com>
2025-08-19 22:30:57 +0100

    portfolio

--- Amenai designs/new.js
+++ Amenai designs/new.js
@@ -1,9 +1,10 @@
  const button = document.getElementById('btn');
 
  button.addEventListener ( "click", function(){
+    confirm('Are you sure?')
      console.log('Welcome');
     HTMLAnchorElement (
-        
+       confirm('Are you sure?') 
     )
 //     confirm('Are you sure?', '<a href="login.html">Login</a>');
      
